cbc885cba4acab5371805e8c15eba3d3d956ee71
TsuchiyaYuki299 <onigiri774@gmail.com>
2025-07-06 03:51:23 +0900

    youtube動画リンクを追加

--- assessment-myself.js
+++ assessment-myself.js
@@ -1,13 +1,13 @@
 let music = [
-  "BOW AND ARROW",
-  "がらくた - JUNK",
-  "Plazma",
-  "KICK BACK",
-  "POP SONG",
-  "死神",
-  "地球儀",
-  "砂の惑星 ( + 初音ミク )",
-  "カムパネルラ",
+  { title: "BOW AND ARROW", url: "https://youtu.be/M-Eyhjkepy0?si=GuXATAl2hgozu_to" }, // ダミーのURLです
+  { title: "がらくた - JUNK", url: "https://youtu.be/2mUC91bXt60?si=8S65Kq9SN6MlQWVv" },
+  { title: "Plazma", url: "https://youtu.be/fp3F6TqBsAU?si=PNLdCGHyq77gw3LV" },
+  { title: "KICK BACK", url: "https://youtu.be/M2cckDmNLMI?si=HjEawp3v1J38kz6u" },
+  { title: "POP SONG", url: "https://youtu.be/mHP6-D-yBEw?si=1q4myzFy0vXCw21U" },
+  { title: "死神", url: "https://youtu.be/8nxaZ69ElEc?si=_A4UMABRt-9us5yM" },
+  { title: "地球儀", url: "https://youtu.be/VUsURj_OYdA?si=N6NdQOz7sLTNNLPT" },
+  { title: "砂の惑星 ( + 初音ミク )", url: "https://youtu.be/AS4q9yaWJkI?si=TafceU_tWSyxPNlB" },
+  { title: "カムパネルラ", url: "https://youtu.be/XeFQJ6-XoD0?si=c_Mmes29hQ2CEYju" },
 ];
 
 // アイテムを表示する場所を見つける
@@ -16,15 +16,11 @@ const randomMusicDisplay = document.getElementById('randomMusic');
 // ランダムにアイテムを選んで表示する関数
 function displayRandomMusic() {
   // 1.ランダムな数字を作る
-  // items.lengthは配列のアイテムの数
-  // Math.random（） は0以上1未満のランダムな少数
-  // それらを掛け算して、Math.floor() で小数点以下を切り捨てると、
-  // 0から1までの整数（アイテムの数 -1 ) がランダムで作られる
   const randomIndex = Math.floor(Math.random() * music.length);
 
   // 2.その数字を使って、配列の中からアイテムを選ぶ
   const selectedMusic = music[randomIndex];
 
   // 3.選んだアイテムをHTMLに表示
-  randomMusicDisplay.textContent = selectedMusic;
+  randomMusicDisplay.innerHTML = `<a href="${selectedMusic.url}" target="_blank">${selectedMusic.title}</a>`;
 }
